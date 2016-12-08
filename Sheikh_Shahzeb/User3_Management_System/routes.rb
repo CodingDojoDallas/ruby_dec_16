04570414217b8ccd8973fbdcbbce537993859e1b
Shahzeb Sheikh <shahzebsheikh54@gmail.com>
2016-12-08 11:37:02 -0600

    User Management 3

---
+++ Sheikh_Shahzeb/User3_Management_System/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+	get '/' =>'users#index'
+	get '/users/new' =>'users#new'
+	post '/users' => 'users#create'
+	get 'users/:id' => 'users#show'
+	get 'users/:id/edit' =>'users#edit'
+	patch 'users/:id' => 'users#update'
+	delete 'users/:id' => 'users#destroy'
+end
