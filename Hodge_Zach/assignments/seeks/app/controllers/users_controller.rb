9063e0e28ee37fe6f9126ed4a6adcb35caa95c5b
amoraq@gmail.com <amoraq@gmail.com>
2016-12-13 06:51:32 -0600

    useres#create Rails II

---
+++ Hodge_Zach/assignments/seeks/app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+    def show
+        @user = User.find(session[:user_id])
+    end
+
+    def new
+        user = User.new
+    end
+
+    private
+        def user_params
+            params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+        end
+end
